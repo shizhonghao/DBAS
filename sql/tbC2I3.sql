9e72e042c4feb95f2970f0d05704e0212608edfe
shizhonghao <shi1996426frank@126.com>
2017-07-08 23:05:41 +0800

    create tabel sql

---
+++ sql/tbC2I3.sql
@@ -0,0 +1,7 @@
+use DBSA
+create table tbC2I3
+(
+A_ID nvarchar(255),
+B_ID nvarchar(255),
+C_ID nvarchar(255)
+)
---
+++ sql/tbC2Inew.sql
@@ -0,0 +1,10 @@
+use DBSA
+create table tbC2Inew
+(
+SCELL nvarchar(255),
+NCELL nvarchar(255),
+C2I_Mean float,
+std float,
+PrbC2I9 float,
+PrbABS6 float,
+)
